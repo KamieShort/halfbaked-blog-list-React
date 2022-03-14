eca29b889625d9f5aeb9f5577c52108db560fb3d
Kamie Short <kamie.short@gmail.com>
2022-03-14 12:41:32 -0700

    added getBlogs function

---
+++ src/views/Main/Main.js
@@ -0,0 +1,2 @@
+// - Main component should have a state variable called `blogs` that defaults to an empty array
+// - Main component should use a `useEffect` hook to call `getBlogs` from the services layer asynchronously and then set the returned data in state
