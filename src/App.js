3e3cabbe041a9963963d7b7c59d0fec9cea227ea
Kamie Short <kamie.short@gmail.com>
2022-03-14 13:16:40 -0700

    blog card displaying

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/layout/Header/Header';
 import Footer from './components/layout/Footer/Footer';
+import Main from './views/Main/Main';
 
 function App() {
   return (
     <section className="main">
       <Header />
+      <Main />
       {/* add your main component here */}
       <Footer />
     </section>
